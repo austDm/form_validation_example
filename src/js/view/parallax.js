1cd4d33164aeca233abf69f22dc0259049e033c7
Dmitry_Aust <Dmitry_Aust@epam.com>
2019-09-09 12:17:18 +0300

    gh page init

--- src/js/view/parallax.js
+++
@@ -1,7 +0,0 @@
-import { birds } from './nodes';
-
-window.addEventListener('scroll', () => {
-  const scroll = window.pageYOffset;
-
-  birds.style.top = `${scroll / 18}rem`;
-});
--- src/js/view/roundabout/roundabout-hide.js
+++
@@ -1,7 +0,0 @@
-import { roundaboutContainer, roundaboutHideCheckbox } from '../nodes';
-
-const hideRoundabout = () => {
-  roundaboutContainer.classList.toggle('roundabout--hide');
-};
-
-roundaboutHideCheckbox.addEventListener('change', hideRoundabout);
